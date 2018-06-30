65bb12ce5021e2b7b11f8153a53c96d174e42870
braelynmuskwe <braemira1@icloud.com>
2018-06-30 18:29:16 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,9 @@
-class Model
-  # Replace with Dog clas
+
+def get_fortune()
+  #method goes here 
+  fortunes_array = ["You'll have a great day", "someone is going to go to the bathroom in 20 minutes", "You'll have sandwhiches for the rest of your life", "you will meet the love of your life next week", "You will get all A's in the next school year" ]
+return fortunes_array.sample
 end
+
+puts get_fortune
+  
